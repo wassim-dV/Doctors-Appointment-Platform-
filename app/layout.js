d776e0070367efc0f67405019a974884157476e3
wassim <wslimani455@gmail.com>
2025-07-09 23:42:12 +0200

    project is done and deployment

---
+++ app/layout.js
@@ -0,0 +1,43 @@
+import { Inter } from 'next/font/google'
+import './globals.css'
+import { ClerkProvider } from '@clerk/nextjs'
+import { Toaster } from 'sonner'
+import Header from '@/components/header'
+import { ThemeProvider } from '@/components/theme-provider'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'Doctors Appointment App',
+  description: 'Connect with doctors anytime, anywhere',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <ClerkProvider>
+      <html lang='en' suppressHydrationWarning>
+        <head>
+          <link rel='icon' href='/logo.png' sizes='any' />
+        </head>
+        <body className={`${inter.className}`}>
+          <ThemeProvider
+            attribute='class'
+            defaultTheme='dark'
+            enableSystem
+            disableTransitionOnChange
+          >
+            <Header />
+            <main className='min-h-screen'>{children}</main>
+            <Toaster richColors />
+
+            <footer className='bg-muted/50 py-12'>
+              <div className='container mx-auto px-4 text-center text-gray-200'>
+                <p>Made with 💗 by RoadsideCoder</p>
+              </div>
+            </footer>
+          </ThemeProvider>
+        </body>
+      </html>
+    </ClerkProvider>
+  )
+}
